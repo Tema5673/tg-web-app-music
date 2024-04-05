2eb7367c1a491f966a6f72f9227c4d4c160d4e12
Tema <public@mail.ru>
2024-04-05 13:47:28 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ function App() {
 
     return (
         <div className="App">
-            <div className="image-container">
-                <img className="left-image" src="/left-image.jpg" alt="Картинка слева" />
-                <img className="center-image" src="/center-image.jpg" alt="Картинка посередине" />
-                <img className="right-image" src="/right-image.jpg" alt="Картинка справа" />
-            </div>
             <header className="center-header">
                 <h1 className="header-text">Выберите жанр</h1>
             </header>
             <div className="genres">
-                <button className="genre-button">Кнопка 1</button>
-                <button className="genre-button">Кнопка 2</button>
-                <button className="genre-button">Кнопка 3</button>
+                <div className="genre">
+                    <img className="genre-image" src="top100.jpg" alt="Топ 100"/>
+                    <h2>Топ 100</h2>
+                </div>
+                <div className="genre">
+                    <img className="genre-image" src="pop.jpg" alt="Поп"/>
+                    <h2>Поп</h2>
+                </div>
+                <div className="genre">
+                    <img className="genre-image" src="rock.jpg" alt="Рок"/>
+                    <h2>Рок</h2>
+                </div>
             </div>
-            {/* Добавьте другие кнопки здесь */}
+            {/* Добавьте другие жанры здесь */}
         </div>
     );
 }
