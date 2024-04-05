6665fcae6739895e8c3e3ee480bdb2e0a0b5ac78
Tema <public@mail.ru>
2024-04-05 13:44:11 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -23,20 +23,11 @@ function App() {
                 <h1 className="header-text">Выберите жанр</h1>
             </header>
             <div className="genres">
-                <div className="genre">
-                    <img className="genre-image" src="/top100.jpg" alt="Топ 100"/>
-                    <h2>Топ 100</h2>
-                </div>
-                <div className="genre">
-                    <img className="genre-image" src="/pop.jpg" alt="Поп"/>
-                    <h2>Поп</h2>
-                </div>
-                <div className="genre">
-                    <img className="genre-image" src="/rock.jpg" alt="Рок"/>
-                    <h2>Рок</h2>
-                </div>
+                <button className="genre-button">Кнопка 1</button>
+                <button className="genre-button">Кнопка 2</button>
+                <button className="genre-button">Кнопка 3</button>
             </div>
-            {/* Добавьте другие жанры здесь */}
+            {/* Добавьте другие кнопки здесь */}
         </div>
     );
 }
