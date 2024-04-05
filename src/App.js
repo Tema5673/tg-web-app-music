00418b8581b3c8b9cee50b50f5bf9a7630708c1c
Tema <public@mail.ru>
2024-04-05 13:49:45 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { useEffect } from "react";
 import './App.css';
-
 const tg = window.Telegram.WebApp;
 
 function App() {
-    useEffect(() => {
-        tg.ready();
-    }, []);
+  useEffect(() => {
+    tg.ready();
+  }, []);
 
-    const onClose = () => {
+  const onClose = () => {
+    // Add your code here
+  }
 
-    }
-
-    return (
-        <div className="App">
-            <header className="center-header">
-                <h1 className="header-text">Выберите жанр</h1>
-            </header>
-            <div className="genres">
-                <div className="genre">
-                    <img className="genre-image" src="top100.jpg" alt="Топ 100"/>
-                    <h2>Топ 100</h2>
-                </div>
-                <div className="genre">
-                    <img className="genre-image" src="pop.jpg" alt="Поп"/>
-                    <h2>Поп</h2>
-                </div>
-                <div className="genre">
-                    <img className="genre-image" src="rock.jpg" alt="Рок"/>
-                    <h2>Рок</h2>
-                </div>
-            </div>
-            {/* Добавьте другие жанры здесь */}
+  return (
+    <div className="App">
+      <header className="center-header">
+        <h1 className="header-text">Выберите жанр</h1>
+      </header>
+      <div className="genres">
+        {/* Remove the lines below */}
+        {/* <div className="genre">
+          <img className="genre-image" src="top100.jpg" alt="Топ 100"/>
+          <h2>Топ 100</h2>
+        </div> */}
+        <div className="genre">
+          <img className="genre-image" src="pop.jpg" alt="Поп"/>
+          <h2>Поп</h2>
         </div>
-    );
+        <div className="genre">
+          <img className="genre-image" src="rock.jpg" alt="Рок"/>
+          <h2>Рок</h2>
+        </div>
+      </div>
+      {/* Добавьте другие жанры здесь */}
+    </div>
+  );
 }
 
 export default App;
