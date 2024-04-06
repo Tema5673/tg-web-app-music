4a613908b443af93668aa4de012542a9ba71744f
Tema <public@mail.ru>
2024-04-06 12:03:45 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import './App.css';
 
 function App() {
+  const inputRef = useRef(null);
+
   useEffect(() => {
-    // Добавьте свой код здесь
+    inputRef.current.focus();
   }, []);
 
   const onClose = () => {
@@ -30,15 +32,15 @@ function App() {
   return (
     <div className="App">
       <div className="center-button">
-        <button className="invisible-button" style={{ width: "350px", height: "30px" }}>Поиск</button>
+        <button className="invisible-button" onClick={onClose} style={{ width: "300px", height: "80px" }}>Поиск</button>
       </div>
-      <header className="center-header" style={{ marginTop: "100px" }}>
+      <header className="center-header" style={{ marginTop: "20px" }}>
         <h1 className="header-text">Выберите жанр</h1>
       </header>
       <div className="genres" style={{ marginTop: "50px" }}>
         <div className="genre">
           <button className="invisible-button" onClick={() => handleButtonClick("Топ 100")}>
-            <img className="genre-image" src="top100.jpg" alt="Топ 100"/>
+            <img className="genre-image" src="топ100.jpg" alt="Топ 100"/>
             <h2>Топ 100</h2>
           </button>
         </div>
@@ -55,6 +57,7 @@ function App() {
           </button>
         </div>
       </div>
+      <input ref={inputRef} type="text" style={{ opacity: 0, position: "absolute", top: 0, left: 0 }} />
     </div>
   );
 }
