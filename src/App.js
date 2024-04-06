81b51bcbc25eaec872b3df362646595d992cf03c
Tema <public@mail.ru>
2024-04-06 11:39:56 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import React, { useEffect } from "react";
-import { TextInput, Keyboard, Button } from "react-native";
 import './App.css';
-const tg = window.telegram.WebApp;
 
 function App() {
   useEffect(() => {
-    tg.ready();
+    // Add your code here
   }, []);
 
   const onClose = () => {
-    // Добавьте свой код здесь
+    // Add your code here
   }
 
   const handleButtonClick = (genre) => {
     console.log(`Кнопка нажата для жанра: ${genre}`);
     switch (genre) {
       case "Топ 100":
-        window.Расположение.href = "top100.html";
+        window.location.href = "top100.html";
         break;
       case "Поп":
-        window.Расположение.href = "pop.html";
+        window.location.href = "pop.html";
         break;
       case "Рок":
-        window.Расположение.href = "rock.html";
+        window.location.href = "rock.html";
         break;
       default:
         break;
     }
-  }
-
-  const handleSearchPress = () => {
-    Keyboard.dismiss(); // Закрываем клавиатуру
-    // Добавьте свой код для открытия клавиатуры Android
   }
 
   return (
@@ -42,34 +35,23 @@ function App() {
       <div className="genres">
         <div className="genre">
           <button className="invisible-button" onClick={() => handleButtonClick("Топ 100")}>
-            <img className="жанровое изображение" src="топ100.jpg" alt="Топ 100"/>
+            <img className="genre-image" src="top100.jpg" alt="Топ 100"/>
             <h2>Топ 100</h2>
           </button>
         </div>
         <div className="genre">
-          <button className="невидимая кнопка" onClick={() => handleButtonClick("Поп")}>
-            <img className="изображение жанра" src="pop.jpg" alt="Поп"/>
+          <button className="invisible-button" onClick={() => handleButtonClick("Поп")}>
+            <img className="genre-image" src="pop.jpg" alt="Поп"/>
             <h2>Поп</h2>
           </button>
         </div>
         <div className="genre">
-          <button className="невидимая кнопка" onClick={() => handleButtonClick("Рок")}>
+          <button className="invisible-button" onClick={() => handleButtonClick("Рок")}>
             <img className="genre-image" src="rock.jpg" alt="Рок"/>
             <h2>Рок</h2>
           </button>
         </div>
       </div>
-      <div>
-        <h2>Ниже находятся три картинки:</h2>
-        <img src="image1.jpg" alt="Image 1" />
-        <img src="image2.jpg" alt="Image 2" />
-        <img src="image3.jpg" alt="Image 3" />
-      </div>
-      <TextInput
-        style={{ marginTop: 20, paddingHorizontal: 10, height: 40, borderColor: 'gray', borderWidth: 1 }}
-        placeholder="Поиск"
-        onPressIn={handleSearchPress}
-      />
     </div>
   );
 }
