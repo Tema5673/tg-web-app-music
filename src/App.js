506adf7f007b0b7dc2de3d023b40446c190385a8
Tema <public@mail.ru>
2024-04-23 13:55:00 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css'; // Подключаем внешний файл стилей
+import './App.css';
 
-function Button({ iconSrc, text }) {
+function Button({ iconSrc, text, onClick }) {
     return (
-        <button className="button">
+        <button className="button" onClick={onClick}>
             <img src={iconSrc} alt="" className="icon" />
             <span className="text">{text}</span>
         </button>
@@ -11,11 +11,15 @@ function Button({ iconSrc, text }) {
 }
 
 function App() {
+    const goToPersonalArea = () => {
+        window.location.href = "/Personal area.html"; // Замените на путь к вашему HTML-файлу Personal area.html
+    };
+
     return (
         <div className="container">
-            <Button iconSrc="1674365337_3-31.png" text="Профиль" />
+            <Button iconSrc="1674365337_3-31.png" text="Профиль" onClick={goToPersonalArea} />
             <Button iconSrc="kisspng-computer-icons-clip-art-openclipart-magnifying-gla-5d31cfdb595331.9238955815635455633659.png" text="Поиск" />
-            <Button iconSrc="unnamed.png" text="Чат" />
+            <Button iconSrc="icons8-чат-64.png" text="Чат" />
         </div>
     );
 }
