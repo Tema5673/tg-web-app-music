076bb5b3e54acd2d1117714b468f998ed0a59fe2
Tema <public@mail.ru>
2024-04-25 23:02:06 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function Button({ iconSrc, text, onClick }) {
+export default function App() {
     return (
-        <button className="button" onClick={onClick}>
-            <img src={iconSrc} alt="" className="icon" />
-            <span className="text">{text}</span>
-        </button>
-    );
-}
-
-function App() {
-    const goToPersonalArea = () => {
-        window.location.href = "/Personal area.html"; // Замените на путь к вашему HTML-файлу Personal area.html
-    };
-
-    return (
-        <div className="container">
-            <Button iconSrc="1674365337_3-31.png" text="Профиль" onClick={goToPersonalArea} />
-            <Button iconSrc="kisspng-computer-icons-clip-art-openclipart-magnifying-gla-5d31cfdb595331.9238955815635455633659.png" text="Поиск" />
-            <Button iconSrc="icons8-чат-64.png" text="Чат" />
+        <div>
+            <h1>Hello</h1>
         </div>
     );
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
