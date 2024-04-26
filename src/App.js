3c1f6c78c78b8318f3d4cfd3f43e20285d52b961
Tema <public@mail.ru>
2024-04-26 07:12:14 +0300

    add file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>Привет, мир!</p>
+      <p>Это некоторый текст.</p>
+    </div>
+  );
+}
+
+export default App;
